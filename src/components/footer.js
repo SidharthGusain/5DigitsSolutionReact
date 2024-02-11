b680d12435a3144a6d2f829b305c11003cc4ae93
gusainsidharth <gusainsidharth@gmail.com>
2024-02-12 01:34:20 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+
+  // scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <footer className="footer-section bg-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5">
+            <h3>About 5Digits Solution</h3>
+            <p>
+              We are a dynamic team of skilled software developers, united by
+              our passion for innovation and excellence. With members from
+              diverse backgrounds in the United Kingdom and India, each
+              specializing in various technologies such as AI, machine learning,
+              robotics, and data analysis.
+            </p>
+          </div>
+
+          <div className="col-md-3 ml-auto">
+            <h3 className="">Links</h3>
+            <ul className="list-unstyled footer-links ">
+              <li>
+                <Link to="/" onClick={scrollToTop}>Home</Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={scrollToTop}>About</Link>
+              </li>
+              <li>
+                <Link to="#">Request a Quote</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row pt-5 mt-5 text-center">
+          <div className="col-md-12">
+            <div className="border-top pt-5">
+              <p>
+                Copyright &copy;
+                <script>document.write(new Date().getFullYear());</script>
+                All rights reserved
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default function Header() {
+
+  // scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <div className="site-mobile-menu site-navbar-target">
+        <div className="site-mobile-menu-header">
+          <div className="site-mobile-menu-close mt-3">
+            <span className="fa fa-xmark js-menu-toggle"></span>
+          </div>
+        </div>
+        <div className="site-mobile-menu-body"></div>
+      </div>
+
+      <header
+        className="site-navbar py-4 js-sticky-header site-navbar-target"
+        role="banner"
+      >
+        <div className="container-fluid">
+          <div className="d-flex align-items-center">
+            <div className="site-logo mr-auto w-25">
+              <Link to="/" onClick={scrollToTop}>
+                <img
+                  src="assets/images/logo2.1.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </Link>
+            </div>
+
+            <div className="mx-auto text-center">
+              <nav
+                className="site-navigation position-relative text-right"
+                role="navigation"
+              >
+                <ul className="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block m-0 p-0">
+                  <li>
+                    <Link to="/" className="nav-link" onClick={scrollToTop}>
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/about" className="nav-link" onClick={scrollToTop}>
+                      About
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="ml-auto w-25">
+              <nav
+                className="site-navigation position-relative text-right"
+                role="navigation"
+              >
+                <ul className="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
+                  <li className="cta">
+                    <Link to="#" className="nav-link" onClick={scrollToTop}>
+                      <span>Request a quote</span>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <Link
+                to="#"
+                className="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"
+              >
+                <span className="fa fa-bars "></span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
